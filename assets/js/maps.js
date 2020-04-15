dfe326e5a7d945ed1dfbd00dbbeac183205ecac3
Rick N <rickmnassar@gmail.com>
2020-04-15 16:26:25 +0000

    Adjusted maps markers,locations and zoom. Added & adjusted cards snippet for mapand jobs result section

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,26 +1,28 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 10,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 53.350140,
+            lng: -6.266155
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
-    ];
-
-    var markers = locations.map(function(location, i) {
+    var markers = locations.map(function (location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
         });
     });
 
+
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
+
+var locations = [
+    { lat: 53.350140, lng: -6.266155 },
+    { lat: 53.350230, lng: -6.261155 },
+    { lat: 53.345140, lng: -6.276155 },
+    { lat: 53.460140, lng: -6.268755 }
+];
