bc37225f120be8a100f296c90e573e8b0ea2264f
Rick N <rickmnassar@gmail.com>
2020-05-02 14:01:48 +0100

    Added Axios request setting & fixed Error: Cannot find module '.config'

--- api/index.js
+++ api/index.js
@@ -1,5 +1,42 @@
-const createServer = require('http').createServer;
-const url = require('url');
-const axios = require('axios');
-const chalk = require('chalk');
-const config = require('.config');
+// Server to run the job search api and dependencies
+const createServer = require("http").createServer;
+const url = require("url");
+const axios = require("axios");
+const chalk = require("chalk");
+const config = require("./config");
+
+const headers = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET",
+};
+
+const server = createServer((req, res) => {
+  const requestURL = url.parse(req.url);
+  const decodedParams = decodeParams(new URLSearchParams(requestURL.search));
+  const { search, location, country = "ie" } = decodedParams;
+
+  // Axios requests & error settings
+  const targetURL = `${config.BASE_URL}/${country.toLowerCase()}/${config.BASE_PARAMS}&app_id=${config.APP_ID}&app_key=${config.API_KEY}&what=${search}&where=${location}`;
+  if (req.method === "GET") {
+    console.log(chalk.blue(`Proxy GET request to : ${targetURL}`));
+    axios
+      .get(targetURL)
+      .then((response) => {
+        res.writeHead(200, headers);
+        res.end(JSON.stringify(response.data));
+      })
+      .catch((response) => {
+        console.log(chalk.red(response));
+        res.writeHead(500, headers);
+        res.end(JSON.stringify(response));
+      });
+  }
+});
+
+
+server.listen(3000, () => {
+  console.log(chalk.green('Server listening'));
+} );
+
+
