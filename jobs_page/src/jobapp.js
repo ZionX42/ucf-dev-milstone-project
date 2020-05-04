6601d13c56a8fddbfc594709417f429a8504a092
Rick N <rickmnassar@gmail.com>
2020-05-04 17:45:13 +0100

    CSS cleanup of code and files

--- jobs_page/src/jobapp.js
+++ jobs_page/src/jobapp.js
@@ -1,6 +1,6 @@
 //This is the main job search JS file that will run the search results
-import {JobSearch} from './jobseach';
+import {JobSearch} from './JobSearch';
 
-const JobSearch = new JobSearch('#search-form', '.result-container', '.loading-element');
+const JobSearch = new JobSearch('#search-form', '.result-container');
 jobSearch.setCountryCode();
 jobSearch.configureFormListener();
--- jobs_page/src/templates.js
+++ jobs_page/src/templates.js
@@ -1,11 +1,11 @@
-// Jobs rearch results template
+// Jobs rearch results template and how it will look in reults page
 export const jobTemplate = (job, currency) => `
 <div class="card">
   <div class="card-body">
-  <h4 class="card-title">${job.title} up to ${currency}${job.salary_max}</h4>
-  <h5>${job.location.display_name}</h5>
-  <p class="card-text">${job.description}</p>
-  <a href="${job.redirect_url}">View Job</a>
+    <h4 class="card-title">${job.title} up to ${currency}${job.salary_max}</h4>
+      <h5>${job.location.display_name}</h5>
+         <p class="card-text">${job.description}</p>
+          <a href="${job.redirect_url}" target="_blank">View Job</a>
   </div>
 </div>
 `;
